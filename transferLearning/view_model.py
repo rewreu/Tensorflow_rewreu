545ad440ce2eedc4b2dc9106c9a7e27d533bfca2
Zhe Wu <zwu@kinetica.com>
2018-01-24 16:20:30 -0800

    no message

--- transferLearning/view_model.py
+++ transferLearning/view_model.py
@@ -1,22 +1,38 @@
-# from retrain import create_image_lists
-import tensorflow as tf
+# to use this,
+# run python view_model.py --model_file model.pb --log_dir /tmp
+# then go to the default port for tensorboard to view your model(normally 6006)
+import argparse
 import os
 import os.path
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
 if __name__ == "__main__":
-    # ret=create_image_lists("./testImages",50,30)
-    INCEPTION_LOG_DIR = '/tmp/tensorflow'
-
-    if not os.path.exists(INCEPTION_LOG_DIR):
-        os.makedirs(INCEPTION_LOG_DIR)
+    FLAGS=None
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model_file',
+        type=str,
+        default='./inception_v3_model/classify_image_graph_def.pb',
+        help='Path to model file, in pb format.'
+    )
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default='/tmp/tensorflow',
+        help='Path to folder to log for tensorboard.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    LOG_DIR = FLAGS.log_dir
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
     with tf.Session() as sess:
-        model_filename = './inception_v3_model/classify_image_graph_def.pb'
+        model_filename = FLAGS.model_file
+
         with gfile.FastGFile(model_filename, 'rb') as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
             _ = tf.import_graph_def(graph_def, name='')
-        writer = tf.summary.FileWriter(INCEPTION_LOG_DIR, tf.get_default_graph())
+        writer = tf.summary.FileWriter(LOG_DIR, tf.get_default_graph())
         writer.close()
-
+    os.system('tensorboard --logdir=' + LOG_DIR)
