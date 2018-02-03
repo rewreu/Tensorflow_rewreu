e2ee48fd3b81de0eee9b608548a61985dadea496
Zhe Wu <zwu@kinetica.com>
2018-02-03 15:12:09 -0500

    no message

--- transferLearning/retrain_test/retrain.batch.py
+++ transferLearning/retrain_test/retrain.batch.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 from IOutil import create_image_lists
 from random import shuffle
+import numpy as np
 
-epochs = 10
+EPOCHS = 10
+BASE_MODEL_PATH = u"../inception_v3_model/classify_image_graph_def.pb"
+IMAGE_DIR = u"../food_dir"
 
 
-def loadBaseModel2Graph(model_file=u"../inception_v3_model/classify_image_graph_def.pb", load_binary=False):
+def loadBaseModel2Graph(model_file=BASE_MODEL_PATH, load_binary=False):
     if load_binary:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(model_file)
@@ -19,14 +22,25 @@ def loadBaseModel2Graph(model_file=u"../inception_v3_model/classify_image_graph_
 
 
 def ModifiedModel(graph, labelsize):
+    """
+
+    :param graph: tenforflow graph loaded from base model
+    :param labelsize: number of catergories to classify
+    :return: graph: modfied graph, input_layer: the endpoint where imagesVec feed into,
+            label: endpoint where label feed into, optimizer, loss,
+            predict: the predict result for training purpose(output vector is for inferencing)
+
+    """
     with graph.as_default():
-        input_layer = graph.get_tensor_by_name("DecodeJpeg/contents:0")
         connectLayer = graph.get_tensor_by_name("pool_3/_reshape:0")
-        weightO = tf.Variable(tf.truncated_normal(shape=(2048, labelsize), stddev=0.01), name="weightOL")
+        input_layer = tf.placeholder(tf.float32, shape=[None, 2048])  # can be multiple at same time
+
+        weightO = tf.Variable(tf.truncated_normal(shape=(2048, labelsize), stddev=0.01), name="weightO")
         biasO = tf.Variable(tf.zeros([labelsize, 1]), name="biasO")
 
-        predict = tf.matmul(connectLayer, weightO) + biasO
+        predict = tf.add(tf.matmul(input_layer, weightO) + biasO)
 
+        output = tf.add(tf.matmul(input_layer, weightO) + biasO, name="outputO")
         label = tf.placeholder(tf.int32, shape=(labelsize, 1))
 
         cross_entropy = tf.losses.sparse_softmax_cross_entropy(logits=predict, labels=label)
@@ -35,31 +49,56 @@ def ModifiedModel(graph, labelsize):
         optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(loss, var_list=[weightO, biasO])
         return graph, input_layer, label, optimizer, loss, predict
 
-def main():
-    base_dir = u"../food_dir"
-    # base_dir = u"/notebooks/TransferLearning/fooddata/food-101/images"
-    imagelist = create_image_lists(base_dir, testing_percentage=10, validation_percentage=20)
+
+def feedVec(offset, batch_size, image_list):
+    images = []
+    labels = []
+    for item in image_list:
+        with open(item[0], 'r') as bottleneck_file:
+            bottleneck_string = bottleneck_file.read()
+        bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+        images.append(bottleneck_values)
+        labels.append(item[1])
+
+    return None
+
+
+def shuffleList(imagelist, base_dir, imageSet="training"):
+    """
+
+    :param imagelist: image list returns from create_image_lists call,
+    :param base_dir: base directory contains all image folders
+    :param imageSet: options available are "training","testing", "validation"
+    :return:
+    """
     encoding = {k: v for v, k in enumerate(imagelist.keys())}  # {"edamame": 0, "omelette": 1}
     category_size = len(encoding)
-    training_list = []
+    image_path_list = []
     for category in imagelist:
         code = encoding[category]
         for image in imagelist[category]["training"]:
-            path = base_dir + "/" + category.replace(" ", "_") + "/" + image
-            training_list.append([path, code])
-    shuffle(training_list)
+            path = base_dir + "/" + imagelist[category]["dir"] + "/" + image
+            image_path_list.append([path, code])
+
+
+    shuffle(image_path_list)
+    return image_path_list
+
+
+def main():
+    base_dir = IMAGE_DIR
+    # base_dir = u"/notebooks/TransferLearning/fooddata/food-101/images"
+    imagelist = create_image_lists(base_dir, testing_percentage=10, validation_percentage=20)
 
     graph2 = loadBaseModel2Graph()
     (graph, input_layer, label, optimizer, loss, predict) = ModifiedModel(graph2, category_size)
 
     biasO = graph.get_tensor_by_name("biasO:0")
-    weightO = graph.get_tensor_by_name("weightOL:0")
-    connectLayer = graph.get_tensor_by_name("pool_3/_reshape:0")
+    weightO = graph.get_tensor_by_name("weightO:0")
     # print "train list is ",training_list
     with tf.Session(graph=graph) as sess:
-        # tf.initialize_all_variables().run()
         tf.global_variables_initializer().run()
-        for step in range(epochs):
+        for step in range(EPOCHS):
             for i, item in enumerate(training_list):
                 with open(item[0], "r") as f:
                     image = f.read()
@@ -69,7 +108,8 @@ def main():
                 _, l, predictions, b0, w0 = sess.run([optimizer, loss, predict, biasO, weightO], feed_dict=feed_dict)
                 if i % 20 == 0:
                     print("loss at step %d is %f" % (step * len(training_list) + i, l))
-                    #print w0[990:1000] monitor the weights change during optimizing
+                    # print w0[990:1000] monitor the weights change during optimizing
+
 
 if __name__ == "__main__":
     main()
--- transferLearning/retrain_test/test.py
+++ transferLearning/retrain_test/test.py
@@ -1,40 +1,55 @@
 import tensorflow as tf
 from IOutil import create_image_lists
-from IOutil import  encodeRecursive
-def loadModel2graph(model_file=u"../inception_v3_model/classify_image_graph_def.pb", load_binary= False):
-#    with tf.Session() as sess:
-        if load_binary:
+from IOutil import encodeRecursive
+
+
+def loadModel2graph(model_file=u"../inception_v3_model/classify_image_graph_def.pb", load_binary=False):
+    #    with tf.Session() as sess:
+    if load_binary:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(model_file)
+    else:
+        with tf.gfile.Open(model_file, 'rb') as f:
             graph_def = tf.GraphDef()
-            graph_def.ParseFromString(model_file)
-        else:
-            with tf.gfile.Open(model_file, 'rb') as f:
-                graph_def = tf.GraphDef()
-                graph_def.ParseFromString(f.read())
-        #tf.import_graph_def(graph_def)
-        _ = tf.import_graph_def(graph_def, name='')
-        graph = tf.get_default_graph()
-        return graph
+            graph_def.ParseFromString(f.read())
+    # tf.import_graph_def(graph_def)
+    _ = tf.import_graph_def(graph_def, name='')
+    graph = tf.get_default_graph()
+    return graph
+
 
 def test_encodeRecursive():
     tf.reset_default_graph()
-    graph=loadModel2graph()
-    with tf.Session(graph= graph) as sess:
-        input=graph.get_tensor_by_name("DecodeJpeg/contents:0")
-        output=graph.get_tensor_by_name("pool_3/_reshape:0")
+    graph = loadModel2graph()
+    with tf.Session(graph=graph) as sess:
+        input = graph.get_tensor_by_name("DecodeJpeg/contents:0")
+        output = graph.get_tensor_by_name("pool_3/_reshape:0")
         encodeRecursive("../testImages", "../encodeImages", sess, input, output)
     print "done"
 
+
 def test_load_image_list():
-    imagelist=create_image_lists("../encodeImages", testing_percentage=20,
-                                 validation_percentage=30, load_raw_image= False)
+    imagelist = create_image_lists("../encodeImages", testing_percentage=20,
+                                   validation_percentage=30, load_raw_image=False)
     # print len(imagelist["omelette"]["training"])
     # print len(imagelist["omelette"]["testing"])
     # print len(imagelist["omelette"]["validation"])
     print imagelist
 
+
+def readVecImage():
+    bottleneck_path = "../encodeImages/cat/cat1.txt"
+    with open(bottleneck_path, 'r') as bottleneck_file:
+        bottleneck_string = bottleneck_file.read()
+        bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+    return bottleneck_values
+
+
 if __name__ == "__main__":
     # test_encodeRecursive()
 
-
     test_load_image_list()
+    bot = readVecImage()
+
+
     print "done"
